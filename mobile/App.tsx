c0f7716c3941335ff50bf669076e67df3904c9a7
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-02 00:37:43 -0300

    main

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SingIn } from './src/screens/Signin'
+import { Loading } from './src/components/Loading';
+
+import { THEME } from "./src/styles/theme"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+   <NativeBaseProvider theme={THEME}>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+      { fontsLoaded ? <SingIn /> : <Loading /> }
+    </NativeBaseProvider>
+  )
+}
