818bfcaaafa070c14c24cf45159a62e109ea4112
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-05 04:53:37 -0300

    main

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -28,6 +28,24 @@ export async function authRoutes(fastify: FastifyInstance) {
 
         const userInfo = userInfoSchema.parse(userData)
 
+        let user = await prisma.user.findUnique({
+            where: {
+                googleId: userInfo.id,
+            }
+        })
+
+        if (!user) {
+            user = await prisma.user.create({
+                data: {
+                    googleId: userInfo.id,
+                    name: userInfo.name,
+                    email: userInfo.email,
+                    avatarUrl: userInfo.picture,
+                }
+            })
+        }
+
+
         return{ userInfo }
     })
 }
