5c812306f8403fbe99ca79abe0b642ec79c3cbbf
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-05 16:21:19 -0300

    main

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,12 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { SingIn } from './src/screens/SingIn'
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+
+import { Routes } from "./src/routes";
 import { Loading } from './src/components/Loading';
 
 import { THEME } from "./src/styles/theme"
-import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 
 export default function App() {
@@ -19,7 +20,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
     />
-      { fontsLoaded ? <SingIn /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   )
