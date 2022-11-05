a93831e7d26c57693e15d38c2928f935cae7dfca
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-05 18:45:19 -0300

    main

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser'
+import { api } from '../services/api'
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -23,7 +24,7 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
-    const [user, setUser] = useState<UserProps>({} as UserProps); 
+    const [user, setUser] = useState<UserProps>({} as UserProps);
     const [isUserLoading, setIsUserLoading] = useState(false)
 
     const [request, response, promptAsync] = Google.useAuthRequest({
@@ -42,11 +43,24 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
         } finally {
             setIsUserLoading(false);
         }
-        
+
     }
 
     async function signInWithGoogle(access_token: string) {
-        console.log('TOKEN DE AUTENTICAÇÃO --->', access_token)
+        try {
+            setIsUserLoading(true)
+
+            const tokenResponse = await api.post('/users', { access_token })
+            api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`
+
+            const userInfoResponse = await api.get('/me')
+            setUser(userInfoResponse.data.user)
+        } catch (error) {
+            console.log(error)
+            throw error
+        } finally {
+            setIsUserLoading(false)
+        }
     }
 
     useEffect(() => {
@@ -57,11 +71,11 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
 
     return (
         <AuthContext.Provider
-        value={{
-            singIn,
-            isUserLoading,
-            user
-        }}>
+            value={{
+                singIn,
+                isUserLoading,
+                user
+            }}>
             {children}
         </AuthContext.Provider>
     )
